d83af51509ffca5b30864a539bfda58a96436cbb
Saish Patil <saishpatil1909@gmail.com>
2025-08-19 11:08:32 +0000

    updated banner

--- easihub-theme/javascripts/discourse/api-initializers/category-header.js
+++ easihub-theme/javascripts/discourse/api-initializers/category-header.js
@@ -1,27 +1,27 @@
 import { apiInitializer } from "discourse/lib/api";
-import { service } from '@ember/service';
-import { SPECIAL_TAGS } from '../../consts';
 
 export default apiInitializer(api => {
   const router = api.container.lookup('service:router');
 
   api.onPageChange(() => {
     const route = router.currentRoute;
-
-    const el = getHeaderDescription();
-    console.debug(route);
-
-    if (!el)
-      return;
-
     if (route.name === 'discovery.category') {
-      console.debug(route);
-      // const category = route.
+      setTimeout(() => {
+        const el = getHeaderDescription();
+        const category = route.attributes?.category;
+        
+        if (el && category) {
+          if (!category.uploaded_logo?.url) {
+            el.classList.add('no-logo');
+          } else {
+            el.classList.remove('no-logo');
+          }
+        }
+      }, 100);
     }
   });
 });
 
-
 function getHeaderDescription() {
-  return document.querySelector('.category-title-contents.category-title-description');
+  return document.querySelector('.category-title-contents .category-title-description');
 }
