e753009b2f5ef2214fa5185bdc42272951970083
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2025-08-19 07:02:55 +0530

    update

---
+++ easihub-theme/javascripts/discourse/templates/code-of-conduct.gjs
@@ -0,0 +1,9 @@
+import RouteTemplate from "ember-route-template";
+
+export default RouteTemplate(
+  <template>
+
+    Code of Conduct
+
+  </template>
+);
--- easihub-theme/javascripts/route-map.js
+++ easihub-theme/javascripts/route-map.js
@@ -2,4 +2,5 @@ export default function () {
   console.log(this);
 
   this.route('about-us', { path: '/about-us-2' });
+  this.route('code-of-conduct', { path: '/code-of-conduct-2' });
 }
